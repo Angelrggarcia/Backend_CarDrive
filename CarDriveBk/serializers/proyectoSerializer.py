294a720353fcc64093f23f916bfe079524cb1630
Sebastián Presno <sebastiandash@icloud.com>
2024-05-12 23:08:01 -0600

    fixed problem regarding migrations

--- CarDriveBk/serializers/proyectoSerializer.py
+++ CarDriveBk/serializers/proyectoSerializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from ..models.proyectos import Proyecto
 
-class ProyectoSerializer(serializers.ModelSerializer):
+class  ProyectoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Proyecto
         fields = '__all__'
