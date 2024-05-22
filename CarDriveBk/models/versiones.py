90a1e02d14384e183161f724bca3165e5598e252
Escudero <A00832309@tec.mx>
2024-05-22 13:01:47 -0600

    Kiubo

--- CarDriveBk/models/versiones.py
+++ CarDriveBk/models/versiones.py
@@ -6,6 +6,9 @@ class Versiones(models.Model):
     id_archivo = models.ForeignKey(Archivos, on_delete=models.CASCADE)
     iteracion = models.IntegerField()
     archivo = models.BinaryField()
+    
+    # archivo = models.BinaryField(null=False)
+    # tamales = models.FileField(upload_to='documentos/', null=True, default="N/A")
 
     def __str__(self):
         return self.id_archivo.__str__() + " - " + self.iteracion.__str__()
--- CarDriveBk/serializers/versionSerializer.py
+++ CarDriveBk/serializers/versionSerializer.py
@@ -1,9 +1,11 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from ..models.versiones import Versiones
 
 
 class VersionSerializer(serializers.ModelSerializer):
+    documents = Base64ImageField(required=False)
     class Meta:
         model = Versiones
         fields = '__all__'
