8c29c348845e182a881e5a3a51810c0ea0ee59ac
Inge Garcia <angel_rgg@hotmail.com>
2024-05-26 11:09:59 -0600

    a

--- CarDriveBk/views/permisosView.py
+++ CarDriveBk/views/permisosView.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from ..models.permisos import Proyectleader, Coordinador, Miembros
 from ..serializers.permisosSerializer import ProyectoLeaderSerializer, CoordinadorSerializer, MiembroSerializer
@@ -7,13 +7,19 @@ from ..serializers.permisosSerializer import ProyectoLeaderSerializer, Coordinad
 class ProyectoleaderView(viewsets.ModelViewSet):
     queryset = Proyectleader.objects.all()
     serializer_class = ProyectoLeaderSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('id_proyecto__id',)
 
 
 class CoordinadoresView(viewsets.ModelViewSet):
     queryset = Coordinador.objects.all()
     serializer_class = CoordinadorSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('id_apartado__id',)
 
 
 class MiembrosView(viewsets.ModelViewSet):
     queryset = Miembros.objects.all()
     serializer_class = MiembroSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('id_apartado__id',)
--- CarDriveBk/views/serviciosView.py
+++ CarDriveBk/views/serviciosView.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from ..models.servicios import Recientes, Favoritos
 from ..serializers.servicioSerializer import RecienteSerializer, FavoritoSerializer
@@ -7,9 +7,13 @@ from ..serializers.servicioSerializer import RecienteSerializer, FavoritoSeriali
 class RecientesView(viewsets.ModelViewSet):
     queryset = Recientes.objects.all()
     serializer_class = RecienteSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('id_usuario__id',)
 
 
 class FavoritosView(viewsets.ModelViewSet):
     queryset = Favoritos.objects.all()
     serializer_class = FavoritoSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('id_usuario__id',)
 
