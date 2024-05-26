d0a4b716b7134a4de884895596c4f87ff5423302
Inge Garcia <angel_rgg@hotmail.com>
2024-05-26 11:09:59 -0600

    a

--- CarDriveBk/views/apartadoView.py
+++ CarDriveBk/views/apartadoView.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets, filters
 
 from ..models.apartado import Apartados
 from ..serializers.apartadosSerializer import ApartadosSerializer
-from rest_framework.response import Response
 
 class ApartadoView(viewsets.ModelViewSet):
     queryset = Apartados.objects.all()  # Add this line
