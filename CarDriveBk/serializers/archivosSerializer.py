bcc534a7d7a4fdb49ac6a232850d9e0be0348587
Escudero <A00832309@tec.mx>
2024-06-10 01:58:38 -0600

    Busquedas

--- CarDriveBk/serializers/archivosSerializer.py
+++ CarDriveBk/serializers/archivosSerializer.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
+from django.db.models import Max
+
 
 from ..models import Usuarios
 from ..models.archivos import Archivos
 from ..models.etiquetas import RelacionesEtiquetas, Etiquetas
 from ..models.servicios import Favoritos
+from ..models.versiones import Versiones
 
 
 class ArchivoSerializer(serializers.ModelSerializer):
     etiquetas = serializers.SerializerMethodField()
+    iteracion = serializers.SerializerMethodField()
     usuario_info = serializers.SerializerMethodField(read_only=True)
+    
     id_usuario = serializers.PrimaryKeyRelatedField(
         write_only=True,
         queryset=Usuarios.objects.all(),
@@ -17,12 +22,22 @@ class ArchivoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Archivos
-        fields = ['id', 'nombre', 'descripcion', 'terminacion', 'fecha', 'usuario_info', 'id_usuario', 'etiquetas', 'favorito', 'id_apartado']
+        fields = ['id', 'nombre', 'descripcion', 'terminacion', 'fecha', 'usuario_info', 'id_usuario', 'etiquetas', 'favorito', 'id_apartado', 'iteracion']
 
     def get_etiquetas(self, obj):
         relaciones = RelacionesEtiquetas.objects.filter(id_archivos=obj)
         etiquetas = [{'nombre': rel.id_etiquetas.nombre, 'color': rel.id_etiquetas.color} for rel in relaciones]
         return etiquetas
+    
+    def get_iteracion(self, obj):
+        busqueda = Versiones.objects.filter(id_archivo=obj)
+        resultado = busqueda.aggregate(Max('iteracion'))
+        iteracion_max = resultado['iteracion__max']
+        
+        if iteracion_max is None:
+            iteracion_max = 0
+    
+        return  iteracion_max
 
     def get_usuario_info(self, obj):
         usuario = obj.id_usuario
