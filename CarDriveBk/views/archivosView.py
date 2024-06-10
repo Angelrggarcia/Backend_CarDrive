0f44293bb6b5aec11f5dced9eea2426231558ece
Escudero <A00832309@tec.mx>
2024-06-10 01:58:38 -0600

    Busquedas

--- CarDriveBk/views/archivosView.py
+++ CarDriveBk/views/archivosView.py
@@ -11,16 +11,35 @@ class ArchivosView(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         search = self.request.query_params.get('search', None)
+        proyecto_id = self.request.query_params.get('id_proyecto', None)
+        proyecto_nombre = self.request.query_params.get('nombre_proyecto', None)
         apartado_id = self.request.query_params.get('id_apartado', None)
+        apartado_nombre = self.request.query_params.get('nombre_apartado', None)
         etiqueta_nombre = self.request.query_params.get('etiqueta', None)
+        fecha_inicio = self.request.query_params.get('fecha_inicio', None)
+        fecha_fin = self.request.query_params.get('fecha_fin', None)
+        persona = self.request.query_params.get('persona', None)
+        # Filtros para proyectos fecha_inicio, fecha_fin, persona 
 
         if search:
             queryset = queryset.filter(nombre__icontains=search)
+        if proyecto_id:
+            queryset = queryset.filter(id_proyecto=proyecto_id)
+        if proyecto_nombre:
+            queryset = queryset.filter(nombre_proyecto=proyecto_nombre)
         if apartado_id:
             queryset = queryset.filter(id_apartado=apartado_id)
+        if apartado_nombre:
+            queryset = queryset.filter(nombre_apartado=apartado_nombre)
         if etiqueta_nombre:
             queryset = queryset.filter(
-                relaciones_etiquetas__id_etiquetas__nombre__icontains=etiqueta_nombre
-            )
+                relaciones_etiquetas__id_etiquetas__nombre__icontains=etiqueta_nombre)
+        if fecha_inicio:
+            queryset = queryset.filter(fecha__gte=fecha_inicio)
+        if fecha_fin:
+            queryset = queryset.filter(fecha__lte=fecha_fin)
+        if persona:
+            queryset = queryset.filter(nombre_persona=persona)  
+            
 
         return queryset
